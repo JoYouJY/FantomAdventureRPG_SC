bffe7251c22df940ea346cfbe08d07eeb9bb7ab3
Jojo <133107674+JoYouJY@users.noreply.github.com>
2023-05-15 18:41:35 +0800

    Update main.sol
    modified battle function to AI and Quest (10 level)

--- Contracts/main.sol
+++ Contracts/main.sol
@@ -61,51 +61,54 @@ contract Main is ERC721Enumerable, ERC721Burnable, Ownable {
     uint16 private constant BATTLESTAMINA = 2 minutes;
     
     uint256 private tokenIdTracker;
+    
+    //TowerLevel 0 is havent start. 1 to 20 is level one.
+    // 21 to 40 is level two, and so on. to 181 to 200 for level 10. 
+    mapping (address => uint8) public TowerLevel; 
+    A.Pets[MAX_MINTABLE] public Pet;
 
-    A.myPets[MAX_MINTABLE] public myPet;
-
-    event Result(uint256 indexed id, bool won, uint256 hash, A.myPets selfOrBefore, A.myPets opponOrAfter, uint64 damage, uint bit);
-    event StatChangedResult(A.myPets AfterMon);
+    event Result(uint256 indexed id, bool won, uint256 hash, A.Pets selfOrBefore, A.Pets opponOrAfter, uint64 damage, uint bit);
+    event StatChangedResult(A.Pets AfterMon);
 
     function safeMint(address to, uint256 tokenId) public onlyOwner {
         _safeMint(to, tokenId);
     }
     //--------------------------------- MINTING FUNCTIONS ------------------------------------
     /**
-    * @dev Mints a new myPet to the specified address.
+    * @dev Mints a new Pet to the specified address.
     *
-    * @param _to The address to mint the myPet to.
+    * @param _to The address to mint the Pet to.
     */
     function _mint(address _to) private {
         // Get the next available token ID.
         uint id = tokenIdTracker;
         tokenIdTracker++;
 
-        // Generate a random number for the myPet's attributes.
-        uint _rand = uint(keccak256(abi.encodePacked(msg.sender, block.gaslimit, block.timestamp)));
+        // Generate a random number for the Pet's attributes.
+        uint _rand = uint(keccak256(abi.encodePacked(msg.sender, block.timestamp,block.coinbase)));
 
-        // Mint the myPet.
-        myPet[id] = core.mintEgg(_rand << ((tokenIdTracker % 50) * 3)); //<<((tokenIdTracker%100)*3)
-        myPet[id].attribute.id = uint16(id);
+        // Mint the Pet.
+        Pet[id] = core.mintEgg(_rand << ((tokenIdTracker % 50) * 3)); //<<((tokenIdTracker%100)*3)
+        Pet[id].attribute.id = uint16(id);
         _safeMint(_to, id);
 
         // Emit a StatChangedResult event.
-        emit StatChangedResult(myPet[id]);
+        emit StatChangedResult(Pet[id]);
     }
 
     /**
-    * @dev Mints multiple myPets to the specified address.
+    * @dev Mints multiple Pets to the specified address.
     *
-    * @param _to The address to mint the myPets to.
-    * @param _count The number of myPets to mint.
+    * @param _to The address to mint the Pets to.
+    * @param _count The number of Pets to mint.
     */
     function mint(address _to, uint256 _count) public payable {
-    // Check that the total number of myPets to mint does not exceed the maximum.
+    // Check that the total number of Pets to mint does not exceed the maximum.
         require((tokenIdTracker + _count <= MAX_MINTABLE) && //error.exceed total MAX mintable
                 (_count <= MAX_PER_ATTEMPT) && //error.exceed multi-mint max limit
                 (msg.value >= _count * MINTPRICE)); //error.less than needed total mint cost
 
-        // Mint the myPets.
+        // Mint the Pets.
         for (uint256 i = 0; i < _count; i++) {
             _mint(_to);
         }
@@ -113,78 +116,52 @@ contract Main is ERC721Enumerable, ERC721Burnable, Ownable {
 
     //----------------------- Raise Functions ---------------------------------------
     function HatchEgg(uint _id) public { //owner,trainer check in function
-        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.gaslimit, block.timestamp)));
-        myPet[_id] = core.HatchEgg(myPet[_id], ownerOf(_id));
+        Pet[_id] = core.HatchEgg(Pet[_id], ownerOf(_id));
     }
-    function feedsmyPet(uint _id, uint8 _foodtype) public payable { //owner,trainer check in function
-        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.gaslimit, block.timestamp)));
-        myPet[_id] = core.FeedmyPet(rand, myPet[_id], _foodtype,ownerOf(_id)); //requirement check on lib
-        emit StatChangedResult(myPet[_id]);
+    function feedsPet(uint _id, uint8 _foodtype) public payable { //owner,trainer check in function
+        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.timestamp)));
+        Pet[_id] = core.FeedPet(rand, Pet[_id], _foodtype,ownerOf(_id)); //requirement check on lib
+        emit StatChangedResult(Pet[_id]);
     }
-    function trainsmyPet(uint _id, uint8 _trainingtype) public { //owner,trainer check in function
-        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.gaslimit, block.timestamp)));
-        myPet[_id] = core.trainmyPet(rand, myPet[_id], _trainingtype,ownerOf(_id)); //requirement check on lib
-        emit StatChangedResult(myPet[_id]);
+    function trainsPet(uint _id, uint8 _trainingtype) public { //owner,trainer check in function
+        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.timestamp)));
+        Pet[_id] = core.trainPet(rand, Pet[_id], _trainingtype,ownerOf(_id)); //requirement check on lib
+        emit StatChangedResult(Pet[_id]);
     }
-    function BattlemyPet(uint _id, uint8 _rank) public {
-        //_rank 0 to 255 based on difficulty
-        //mysterious forest has 15 level. 
+    function BattlePet(uint _id, uint8 _rank) public {
+        //_rank 0~3 is AI based on self CP. 
+        //_rank 4 = mysterious tower has 10 level.
         uint64 _timenow = uint64(block.timestamp);
         require(msg.sender == ownerOf(_id)  &&
-                _timenow - myPet[_id].time.stamina >= BATTLESTAMINA && 
-                myPet[_id].status == 0 &&
-                myPet[_id].time.deadtime > _timenow && myPet[_id].time.endurance > _timenow ); //Alive
+                _timenow - Pet[_id].time.stamina >= BATTLESTAMINA && 
+                Pet[_id].time.deadtime > _timenow && Pet[_id].time.endurance > _timenow ); //Alive
         bool Mon1Win;
         uint BattleRhythm;
         uint8 bit; // how many bit has been filled for Rythm
         uint64 damage; //dealt total damage to Mon2
-        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.gaslimit, block.timestamp)));
+        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.timestamp)));
         uint16 Startcount;
-        uint16 Endcount;
-        A.myPets memory BattlingmyPet;
-        if (rand%3 == 0) {
-            //rand go increment
-            Startcount = uint16(rand%tokenIdTracker);
-            if (myPet.length - Startcount > 1000) {
-                Endcount = Startcount+1000; //cap at max 1000 loop
-            } else {Endcount = uint16(tokenIdTracker);}
-            for(uint16 i=Startcount; i<Endcount; i++){
-                if (myPet[i].attribute.stage == _rank+1 && i != _id ) { //right rank and not self
-                    BattlingmyPet = myPet[i];
-                    Mon1Win = true; //tag along Mon1Win bool to reduce stack //skip generating AI
-                    break;
-                }
-            }
-        } else if (rand%3 ==1)
-        {
-            //rand go decrement
-            Startcount = uint16(rand%tokenIdTracker);
-            if (Startcount > 1000) {
-                Endcount = Startcount - 1000; //cap at max 1000 loop
-            } else {Endcount = 0;}
-            for(uint16 i=Startcount; i> Endcount; i--){
-                if (myPet[i].attribute.stage == _rank+1 && i != _id ) {//right rank and not self
-                    BattlingmyPet = myPet[i];
-                    Mon1Win = true; //tag along Mon1Win bool to reduce stack //skip generating AI
-                    break;
-                }
-            }
+
+        A.Pets memory BattlingPet;
+        if (_rank <= 3) { //tag along with Mon1Win to reduce stack
+            BattlingPet = core.battlingPet(_rank,rand);
+        } else {
+            BattlingPet = core.TowerPet(TowerLevel,rand);
         }
+        (Mon1Win,BattleRhythm, bit, damage) = core.battlePet(rand, Pet[_id], BattlingPet);
+
+
+        A.Pets memory BattlingPet;
         if (Mon1Win == false) { //tag along with Mon1Win to reduce stack
-            BattlingmyPet = core.battlingmyPet(_rank,rand);
+            BattlingPet = core.battlingPet(TowerLevel,rand);
         }
-        (Mon1Win,BattleRhythm, bit, damage) = core.battlemyPet(rand, myPet[_id], BattlingmyPet);
-        myPet[_id] = core.battlewinlosereward(myPet[_id], Mon1Win, _rank, rand); //exp stars gain   
-        myPet[_id].time.stamina += BATTLESTAMINA; // take up stamina
-        emit Result(_id, Mon1Win, BattleRhythm, myPet[_id], BattlingmyPet,damage, bit); //done battle
+        (Mon1Win,BattleRhythm, bit, damage) = core.battlePet(rand, Pet[_id], BattlingPet);
+        Pet[_id] = core.battlewinlosereward(Pet[_id], Mon1Win, _rank, rand); //exp stars gain   
+        Pet[_id].time.stamina += BATTLESTAMINA; // take up stamina
+        emit Result(_id, Mon1Win, BattleRhythm, Pet[_id], BattlingPet,damage, bit); //done battle
         
     }
-    function BattleSimulationR(A.myPets memory Mon1, A.myPets memory Mon2) public view //anyone can call -- for external contract
-    returns (bool Mon1Win, uint256 BattleRhythm, uint64 damage, uint8 bit ) {//for collaboration with other contract
-        uint rand = uint(keccak256(abi.encodePacked(msg.sender, block.gaslimit, block.timestamp)));
-        (Mon1Win,BattleRhythm,bit,damage) = core.battlemyPet(rand, Mon1, Mon2);
-    }
-    
+  
     
 
 
